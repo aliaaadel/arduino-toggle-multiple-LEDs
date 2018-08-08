699f4b35036aba7d33b772bccf8bb27038e6a9ce
Aliaa Adel <35958413+aliaaadel@users.noreply.github.com>
2018-08-08 12:19:29 +0200

    just adjusting some stuff

--- toggle.ino
+++ toggle.ino
@@ -1,50 +1,29 @@
-#define led1 2
-#define led2 3
-#define led3 4
+char LED[]={2,3,4};
 #define button 6
 int buttonstate;
-int laststate;
 int ledstate;
  
  void setup() {
   // put your setup code here, to run once:
-  pinMode(led1,OUTPUT);
-  pinMode(led2,OUTPUT);
-  pinMode(led3,OUTPUT);
+  for (i = 0 ; i<3; i++){
+   pinMode(LED[i], INPUT)
   pinMode(button,INPUT);
 }
 
 void loop() {
-  buttonstate = digitalRead(button);
+  buttonstate = digitalRead(button); //that's the buutton debounce
   if (buttonstate == 1){
     delay(30);
     buttonstate = digitalRead(button);
     if (buttonstate == 0){
        ledstate++;
     }
-    else{
-      ledstate=0
-    }
+    
   }
   else{
     delay(100);}
-  if (ledstate == 1){
-    digitalWrite(led1,HIGH);
-    digitalWrite(led2,LOW);
-    digitalWrite(led3,LOW);
-  }
-  if(ledstate == 2){
-    
-    digitalWrite(led1,LOW);
-    digitalWrite(led2,HIGH);
-    digitalWrite(led3,LOW);
-  }
-  if (ledstate == 3){
-    
-    digitalWrite(led1,LOW);
-    digitalWrite(led2,LOW);
-    digitalWrite(led3,HIGH);
-    ledstate = 0;
-  }
+  whille(ledstate<4){
+     digitalWrite()}
+
 }
   
